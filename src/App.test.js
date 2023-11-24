1c901885e977315598f3ab3ebcf46052b9194223
gagle1231 <kke1231@naver.com>
2023-11-24 09:54:50 +0900

    README

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTextId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
